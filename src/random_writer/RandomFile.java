32b6962968b57c40c69c723bf6db6e4723598393
Parker Hall <parkerh@mit.edu>
2017-07-19 20:13:13 -0700

    Added comments to RandomFile and tinkered with implementation

--- src/random_writer/RandomFile.java
+++ src/random_writer/RandomFile.java
@@ -9,8 +9,14 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-// Represents a probability distribution among English words used in documents
+/*
+Represents a probability distribution among English words used in documents
+Words are stored with trailing punctuation other than quotation marks
+*/
+
 public class RandomFile {
+    
+    private static final int OFFSET = 2;
     
     private static final Set<Character> QUOTES = new HashSet<>(Arrays.asList('\'', '\"'));
     private static final Set<Character> PUNCTUATION = new HashSet<>(Arrays.asList('.', '?', '!'));
@@ -49,33 +55,48 @@ public class RandomFile {
         probabilities = dist;
     }
     
+    /**
+     * Combine two RandomFile distributions
+     * @param that RandomFile to add to this
+     * @return new RandomFile, the combination of this and that
+     */
     public RandomFile combine(RandomFile that) {
         StringDistribution newDist = this.probabilities.combine(that.probabilities);
         return new RandomFile(newDist);
     }
     
+    /**
+     * Add a single word to the distribution
+     * @param word new instance of word to be added
+     */
     public void addWord(String word) {
         String lower = word.toLowerCase();
         String withoutPunc = removePunc(lower);
         probabilities = probabilities.addSingle(withoutPunc);
     }
     
+    /**
+     * Strips input of quotation marks
+     * @param input English word to be modified
+     * @return new String that is input without leading or trailing quotation marks
+     */
     public static String removePunc(String input) {
-        String output = input;
-        int length = output.length();
-        while (QUOTES.contains(output.charAt(0))) {
-            output = output.substring(1, length);
-            length--;
+        int length = input.length();
+        
+        int start = 0;
+        while (QUOTES.contains(input.charAt(start))) {
+            start++;
         }
-        while (QUOTES.contains(output.charAt(length - 1))) {
-            output = output.substring(0, length - 1);
-            length--;
+        
+        int end = length - 1;
+        while (QUOTES.contains(input.charAt(end))) {
+            end--;
         }
-        return output;
+        return input.substring(start, end + 1);
     }
     
     /**
-     * creates randomized String equal in word length to input
+     * creates randomized String with numWords words
      * @param numWords number of words in output String
      * @return String of randomized words separated by whitespace
      */
@@ -83,7 +104,8 @@ public class RandomFile {
         String output = "";
         for (int i = 0; i < numWords; i++) {
             String newWord = probabilities.randomSelect();
-            if (output.length() > 2 && PUNCTUATION.contains(output.charAt(output.length() - 2)) || output.length() == 0) {
+            // if start of new sentence, capitalize next word
+            if (output.length() == 0 || PUNCTUATION.contains(output.charAt(output.length() - OFFSET))) {
                 newWord = Character.toUpperCase(newWord.charAt(0)) + newWord.substring(1);
             }
             output += newWord + " ";
@@ -97,7 +119,11 @@ public class RandomFile {
     }
     
     public static void main(String[] args) {
-        RandomFile check = new RandomFile("Hey everyone! How is everyone doing? I'd like to have a little chat.");
-        System.out.println(check.randomOutput(12));
+        try {
+            RandomFile check = new RandomFile(new File("src/random_writer/testFile2"));
+            System.out.println(check.randomOutput(20));
+        } catch(FileNotFoundException fnfe) {
+            System.err.println("You done messed up now A-A-ron");
+        }
     }
 }
